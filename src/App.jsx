10d3ab050c8b37422df4aeb63088db80b33df914
seanuleh <seanelsayed@gmail.com>
2024-06-13 03:08:52 +1000

    Find and approve pending deployments

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import './App.css'
 import TokenInput from './TokenInput';
 import WorkflowInput from './WorkflowInput';
 import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
+import { parseWorkflowUrl } from './utils';
 
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import DoneIcon from '@mui/icons-material/Done';
+import IconButton from '@mui/material/IconButton';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 
 function App() {
   const [token, setToken] = useState('');
   const [workflow, setWorkflow] = useState('');
+  const [isApproving, setIsApproving] = useState([]);
+  const [deployments, setDeployments] = useState([]);
 
   const handleTokenReceived = (receivedToken) => {
     setToken(receivedToken);
@@ -22,12 +30,83 @@ function App() {
       localStorage.removeItem('token');
       setToken('');
       setWorkflow('');
+      setDeployments([]);
     }
   };
 
   const handleClearWorkflow = () => {
     setWorkflow('');
+    setIsApproving([]);
+    setDeployments([]);
   };
+
+  const fetchDeployments = async () => {
+    try {
+      const { org, repo, run_id } = parseWorkflowUrl(workflow);
+      const response = await fetch(
+        `https://api.github.com/repos/${org}/${repo}/actions/runs/${run_id}/pending_deployments`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      const pendingDeployments = data.map(
+        (deployment) => {
+          return { ...deployment, env_name: deployment.environment.name };
+        }
+      );
+      setDeployments(pendingDeployments);
+
+      // Create a Map to track which Runs we are approving
+      const approvingDeployments = pendingDeployments.map((deployment) => {
+        return {
+          environment: deployment.env_name,
+          status: "pending"
+        }
+      })
+      setIsApproving(approvingDeployments);
+      return pendingDeployments;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  const approveWorkflow = async (env_name, environment_id) => {
+    const { org, repo, run_id } = parseWorkflowUrl(workflow);
+    try {
+      const response = await fetch(
+        `https://api.github.com/repos/${org}/${repo}/actions/runs/${run_id}/pending_deployments`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ 
+            environment_ids: [environment_id], 
+            state: "approved",
+            comment: "LGTM"
+          }),
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      if (response.ok) {
+        setIsApproving(isApproving.map((a) => a.environment === env_name ? { ...a, status: "approving" } : a));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  useEffect(() => {
+    if (token && workflow) {
+      fetchDeployments();
+    }
+  }, [token, workflow]);
+
 
   return (
     <div>
@@ -38,11 +117,6 @@ function App() {
               Clear Token
             </Button>
           )}
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p style={{ marginRight: '1rem', textAlign: 'center' }}>
-              {workflow}
-            </p>
-          </div>
           {workflow && (
             <Button variant="outlined" onClick={handleClearWorkflow}>
               Clear Workflow
@@ -59,9 +133,38 @@ function App() {
       {token && !workflow && (
         <WorkflowInput onWorkflowReceived={handleWorkflowReceived} />
       )}
-      {token && workflow && (
-        <p>Go Fetch Shit</p>
+
+      {workflow && (
+        <>
+            <Typography variant="h6">
+              {parseWorkflowUrl(workflow).org}/{parseWorkflowUrl(workflow).repo}
+            </Typography>
+            <Typography variant="subtitle2">
+              Run ID:
+            </Typography>
+            <Typography variant="subtitle1">
+              {parseWorkflowUrl(workflow).run_id}
+            </Typography>
+        </>
       )}
+
+      {deployments && deployments.map((deployment) => (
+        <>
+          <ListItem key={deployment.env_name} spacing={5}>
+            <ListItemText primary={deployment.env_name} />
+            {isApproving.find((a) => a.environment === deployment.env_name && a.status === "pending") ? (
+              <IconButton aria-label="approve" onClick={() => {
+                approveWorkflow(deployment.env_name, deployment.environment.id);
+              }}>
+                <DoneIcon />
+              </IconButton>
+            ) : null}
+            {isApproving.find((a) => a.environment === deployment.env_name && a.status === "approving") && (
+              <HourglassBottomIcon />
+            )}
+          </ListItem>
+        </>
+      ))}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
